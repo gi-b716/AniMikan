4ee9bf1c978c5793df76456bb51e6fe2f196b845
Gavin <sganyunchi@163.com>
2026-06-13 15:11:24 +0800

    build(android): use mirrors

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,10 @@
 allprojects {
     repositories {
+        // Aliyun
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        
         google()
         mavenCentral()
     }
